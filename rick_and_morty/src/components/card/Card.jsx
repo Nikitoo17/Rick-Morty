46b0359c22852771c471b57fff2724eff7aa06b7
Nikitoo17 <nicolasfadel17@gmail.com>
2023-03-08 13:31:01 -0300

    arreglos card

--- rick_and_morty/src/components/card/Card.jsx
+++ rick_and_morty/src/components/card/Card.jsx
@@ -7,14 +7,14 @@ export default function Card({ name, species, gender, image, onClose }) {
         className={styles.CardImage}
         style={{ backgroundImage: `url(${image})` }}
       >
-        <button onClick={onClose} className={styles.Button}>
-          X
-        </button>
-        <h2 className={styles.CardName}>{name}</h2>
-      </div>
-      <div className={styles.Date}>
-        <h2>{species}</h2>
-        <h2>{gender}</h2>
+        <div className={styles.CardInfo}>
+          <button onClick={onClose} className={styles.Button}>
+            X
+          </button>
+          <h3>{name}</h3>
+          <h3>{species}</h3>
+          <h3>{gender}</h3>
+        </div>
       </div>
     </div>
   );
