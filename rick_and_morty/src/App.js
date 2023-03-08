5080779a3a2e80bdd31830d7b166593949177115
Nikitoo17 <nicolasfadel17@gmail.com>
2023-03-08 09:30:40 -0300

    Modulo App

--- rick_and_morty/src/App.js
+++ rick_and_morty/src/App.js
@@ -5,48 +5,13 @@ import characters from "./data.js";
 
 function App() {
   return (
-    <div
-      style={{
-        padding: "25px 25px",
-        display: "flex",
-        flexDirection: "column",
-        gap: "2em",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div
-          style={{
-            backgroundImage: `url(https://cdn.shopify.com/s/files/1/0346/8063/5529/collections/rick-morty-collection-banner_1400x.jpg?v=1590095280)`,
-            backgroundSize: "100%",
-            width: "50%",
-            height: "15vh",
-          }}
-        ></div>
+    <div className="App">
+      <div className="BannerContainer">
+        <div className="Banner"></div>
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
+      <div className="SearchBar">
         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
       </div>
-      <div
-        style={{
-          padding: "0px",
-          margin: "0px",
-          width: "auto",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      ></div>
       <div>
         <Cards characters={characters} />
       </div>
