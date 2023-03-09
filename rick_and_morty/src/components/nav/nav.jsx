1583d4de259c22a2ab5271da3371034f9ef8a1da
Nikitoo17 <nicolasfadel17@gmail.com>
2023-03-08 21:01:23 -0300

    Agregar random

--- rick_and_morty/src/components/nav/nav.jsx
+++ rick_and_morty/src/components/nav/nav.jsx
@@ -1,8 +1,21 @@
 import SearchBar from "../search/SearchBar";
+import styles from "./nav.module.css";
 
 export default function NavBar(props) {
+  function randomNum() {
+    const randomNumber = Math.floor(Math.random() * 826) + 1;
+    return randomNumber;
+  }
   return (
-    <div>
+    <div className={styles.nav}>
+      <button
+        className={styles.random}
+        onClick={() => {
+          props.value(randomNum());
+        }}
+      >
+        Random{" "}
+      </button>
       <SearchBar onSearch={props.value} />
     </div>
   );
