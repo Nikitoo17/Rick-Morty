76c2f5c06e0b8528db82f3bb4270841144de278a
Nikitoo17 <nicolasfadel17@gmail.com>
2023-03-10 16:20:10 -0300

    add view details

---
+++ rick_and_morty/src/components/detail/DetailID.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./DetailID.module.css";
+export default function DetailID() {
+  const [character, setCharacter] = useState({});
+  const { detailID } = useParams();
+
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/${detailID}`)
+      .then((response) => response.json())
+      .then((data) => setCharacter(data));
+  }, [detailID]);
+
+  return (
+    <div className={styles.main}>
+      {character.name ? (
+        <>
+          <img className={styles.image} src={character.image} alt="" />
+          <div className={styles.details}>
+            <h3>Name : {character.name}</h3>
+            <h3>Status : {character.status}</h3>
+            <h3>Specie : {character.species}</h3>
+            <h3>Gender : {character.gender}</h3>
+            <h3>Origin : {character.origin?.name}</h3>
+          </div>
+        </>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
+  );
+}
---
+++ rick_and_morty/src/view/Detail.jsx
@@ -0,0 +1,4 @@
+import DetailID from "../components/detail/DetailID.jsx";
+export default function Detail() {
+  return <DetailID />;
+}
